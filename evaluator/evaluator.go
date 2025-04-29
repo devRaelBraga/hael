78b77a3288b5f2c4e7afdb3eeb51063211eeb6aa
Hisrael <hisrael.braga@precojusto.app.br>
2025-04-29 16:56:06 -0400

    feat: added bang prefix expression

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -8,6 +8,7 @@ import (
 var (
 	GOOD = &object.Boolean{Value: true}
 	BAD  = &object.Boolean{Value: false}
+	NULL = &object.Null{}
 )
 
 func Eval(node ast.Node) object.Object {
@@ -17,14 +18,39 @@ func Eval(node ast.Node) object.Object {
 		return evalStatements(node.Statements)
 	case *ast.ExpressionStatement:
 		return Eval(node.Expression)
-
 	case *ast.IntegerLiteral:
 		return &object.Integer{Value: node.Value}
 	case *ast.Boolean:
 		return nativeBooleanObject(node.Value)
+	case *ast.PrefixExpression:
+		right := Eval(node.Right)
+		return evalPrefixExpression(node.Operator, right)
+
 	}
 
 	return nil
+}
+
+func evalPrefixExpression(operator string, right object.Object) object.Object {
+	switch operator {
+	case "!":
+		return evalBangOperatorExpression(right)
+	default:
+		return NULL
+	}
+}
+
+func evalBangOperatorExpression(right object.Object) object.Object {
+	switch right {
+	case GOOD:
+		return BAD
+	case BAD:
+		return GOOD
+	case NULL:
+		return GOOD
+	default:
+		return BAD
+	}
 }
 
 func evalStatements(stmts []ast.Statement) object.Object {
--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -37,6 +37,25 @@ func TestEvalBooleanExpression(t *testing.T) {
 	}
 }
 
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!good", false},
+		{"!bad", true},
+		{"!5", false},
+		{"!!good", true},
+		{"!!bad", false},
+		{"!!5", true},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
 func testEval(input string) object.Object {
 	l := lexer.New(input)
 	p := parser.New(l)
